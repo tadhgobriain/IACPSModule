905d46a28924f16676f4f1366993bcb8e3982ec5
Tadhg O Briain <tadhg.obriain@live.com>
2019-06-04 16:28:55 +0100

    Adding functionality for ESXI VM creation

--- TOBInfraAsCode/1.0/TOBInfraAsCode.psm1
+++ TOBInfraAsCode/1.0/TOBInfraAsCode.psm1
@@ -62,81 +62,125 @@ Function New-VMInstance {
             $HypervisorGroup = $CMDB.Hypervisors | Where-Object {$_.Machine.Name -Like $Machine.Hypervisor}
             $Hypervisor = $HypervisorGroup.Machine | Where-Object Name -Like $Machine.Hypervisor #Catch non-exist error
 
-            Switch -Wildcard ( $Machine.MemoryStartupBytes ){
-                '*MB' { $MemoryStartupBytes = [int64]$($Machine.MemoryStartupBytes).Replace('MB','') * 1MB }
-                '*GB' { $MemoryStartupBytes = [int64]$($Machine.MemoryStartupBytes).Replace('GB','') * 1GB }
-                '*TB' { $MemoryStartupBytes = [int64]$($Machine.MemoryStartupBytes).Replace('TB','') * 1TB }
-                Default { Write-Error -Message "No unit (MB/GB/TB) associated with memory allocation for $Computer in $ConfigurationFile"}
-            }
-
-            $Generation = [int16]$($Machine.Generation)
-            $CPUCount = [int64]$($Machine.CPUCount)
-
             Switch -Wildcard ($Hypervisor.Name) {
                 "CompHypV*" { 
+                    Switch -Wildcard ( $Machine.MemoryStartupBytes ){
+                        '*MB' { $MemoryStartupBytes = [int64]$($Machine.MemoryStartupBytes).Replace('MB','') * 1MB }
+                        '*GB' { $MemoryStartupBytes = [int64]$($Machine.MemoryStartupBytes).Replace('GB','') * 1GB }
+                        '*TB' { $MemoryStartupBytes = [int64]$($Machine.MemoryStartupBytes).Replace('TB','') * 1TB }
+                        Default { Write-Error -Message "No unit (MB/GB/TB) associated with memory allocation for $Computer in $ConfigurationFile"}
+                    }
+        
+                    $Generation = [int16]$($Machine.Generation)
+                    $CPUCount = [int64]$($Machine.CPUCount)
+
                     $HypervisorFQDN = $Hypervisor.Name + '.' + $Domain
                     $ConfigPath = $Hypervisor.ConfigPath
-                    $VHDPath = $Hypervisor.VHDPath                    
+                    $VHDPath = $Hypervisor.VHDPath
+                    
+                    If ($PSCmdlet.ShouldProcess($Computer)) {
+                        #VM
+                        Write-Output "Creating new VM instance: $Computer on $($Hypervisor.Name)"
+                        Write-Verbose "Memory: $MemoryStartupBytes"
+                        Write-Verbose "CPU Count: $CPUCount"
+                        Write-Verbose "Generation: $Generation"
+                        Write-Verbose "Boot Device: $($Machine.BootDevice)"
+                        Write-Verbose "Path: $ConfigPath"
+                        Write-Verbose "Switch: $($Machine.DefaultNIC.vSwitch)"
+                        Write-Verbose "Hypervisor: $HypervisorFQDN"
+                        
+                        Hyper-V\New-VM -Name $Computer -MemoryStartupBytes $MemoryStartupBytes -Generation $Generation -NoVHD -BootDevice $Machine.BootDevice -Path $ConfigPath -SwitchName $Machine.DefaultNIC.vSwitch -ComputerName $HypervisorFQDN
+                        Set-VMProcessor -VMName $Computer -Count $CPUCount -ComputerName $HypervisorFQDN
+                        
+                        #Default NIC
+                        If ( $Machine.DefaultNIC.MACType -eq 'Static') {
+                            Write-Verbose "Setting default NIC to 'STATIC' with MAC address $($Machine.DefaultNIC.Mac)"
+                            Set-VMNetworkAdapter -VMName $Computer -StaticMacAddress $Machine.DefaultNIC.Mac -DhcpGuard $Machine.DefaultNIC.DHCPGuard -RouterGuard $Machine.DefaultNIC.RouterGuard -MacAddressSpoofing $Machine.DefaultNIC.MacAddressSpoofing -ComputerName $HypervisorFQDN
+                        }
+                        Else {
+                             Set-VMNetworkAdapter -VMName $Computer -DhcpGuard $Machine.DefaultNIC.DHCPGuard -RouterGuard $Machine.DefaultNIC.RouterGuard -MacAddressSpoofing $Machine.DefaultNIC.MacAddressSpoofing -ComputerName $HypervisorFQDN 
+                        }
+                        
+                        Write-Verbose "DHCP Guard: $($Machine.DefaultNIC.DHCPGuard)"
+                        Write-Verbose "Router Guard: $($Machine.DefaultNIC.RouterGuard)"
+                        Write-Verbose "MAC Address Spoofing: $($Machine.DefaultNIC.MacAddressSpoofing)"
+        
+                        #VHDs
+                        ForEach ($VHD in $Machine.VHDs) {
+                            Write-Output "Creating new VHD(s) for VM instance: $Computer"  
+                            $ControllerLocation = [Int32]$($VHD.ControllerLocation)
+        
+                            Switch -Wildcard ( $VHD.SizeBytes ){
+                                '*GB' { $SizeBytes = [uint64]$($VHD.SizeBytes).Replace('GB','') * 1GB }
+                                '*TB' { $SizeBytes = [uint64]$($VHD.SizeBytes).Replace('TB','') * 1TB }
+                                Default { Write-Error -Message "No unit (MB/GB/TB) associated with VHD size in JSON for $Computer"}
+                            }
+        
+                            If ($VHD.Type -eq 'Fixed') { 
+                                New-VHD -Path "$VHDPath\$Computer\$Computer-$($VHD.Name).vhdx" -SizeBytes $SizeBytes -Fixed -ComputerName $HypervisorFQDN  
+                            }
+                            Else { New-VHD -Path "$VHDPath\$Computer\$Computer-$($VHD.Name).vhdx" -SizeBytes $SizeBytes -ComputerName $HypervisorFQDN }
+        
+                            Add-VMHardDiskDrive -VMName $Computer -Path "$($Hypervisor.VHDPath)\$Computer\$Computer-$($VHD.Name).vhdx" -ControllerLocation $ControllerLocation -ComputerName $HypervisorFQDN
+                        
+                        # Need to code for multiple NICs
+                        }
+                    }
                 }
-                "CompVMW*" {
-
+                "PC-ESXI*" {
+                    $MemoryGB = [decimal]$($Machine.NumCPU)
+                    $NumCPU = [int32]$($Machine.NumCPU)
+                    
+                    If ($PSCmdlet.ShouldProcess($Computer)) {
+                        #VM
+                        Write-Output "Creating new VM instance: $Computer on $($Hypervisor.Name)"
+                        Write-Verbose "Memory: $MemoryGB"
+                        Write-Verbose "CPU Count: $NumCPU"
+                        
+                        VMware.VimAutomation.Core\New-VM -Name $Computer -MemoryGB $MemoryGB -NumCpu $NumCPU -GuestId $Machine.GuestID
+                        <#Set-VMProcessor -VMName $Computer -Count $CPUCount -ComputerName $HypervisorFQDN
+                        
+                        #Default NIC
+                        If ( $Machine.DefaultNIC.MACType -eq 'Static') {
+                            Write-Verbose "Setting default NIC to 'STATIC' with MAC address $($Machine.DefaultNIC.Mac)"
+                            Set-VMNetworkAdapter -VMName $Computer -StaticMacAddress $Machine.DefaultNIC.Mac -DhcpGuard $Machine.DefaultNIC.DHCPGuard -RouterGuard $Machine.DefaultNIC.RouterGuard -MacAddressSpoofing $Machine.DefaultNIC.MacAddressSpoofing -ComputerName $HypervisorFQDN
+                        }
+                        Else {
+                             Set-VMNetworkAdapter -VMName $Computer -DhcpGuard $Machine.DefaultNIC.DHCPGuard -RouterGuard $Machine.DefaultNIC.RouterGuard -MacAddressSpoofing $Machine.DefaultNIC.MacAddressSpoofing -ComputerName $HypervisorFQDN 
+                        }
+                        
+                        Write-Verbose "DHCP Guard: $($Machine.DefaultNIC.DHCPGuard)"
+                        Write-Verbose "Router Guard: $($Machine.DefaultNIC.RouterGuard)"
+                        Write-Verbose "MAC Address Spoofing: $($Machine.DefaultNIC.MacAddressSpoofing)"
+        
+                        #VHDs
+                        ForEach ($VHD in $Machine.VHDs) {
+                            Write-Output "Creating new VHD(s) for VM instance: $Computer"  
+                            $ControllerLocation = [Int32]$($VHD.ControllerLocation)
+        
+                            Switch -Wildcard ( $VHD.SizeBytes ){
+                                '*GB' { $SizeBytes = [uint64]$($VHD.SizeBytes).Replace('GB','') * 1GB }
+                                '*TB' { $SizeBytes = [uint64]$($VHD.SizeBytes).Replace('TB','') * 1TB }
+                                Default { Write-Error -Message "No unit (MB/GB/TB) associated with VHD size in JSON for $Computer"}
+                            }
+        
+                            If ($VHD.Type -eq 'Fixed') { 
+                                New-VHD -Path "$VHDPath\$Computer\$Computer-$($VHD.Name).vhdx" -SizeBytes $SizeBytes -Fixed -ComputerName $HypervisorFQDN  
+                            }
+                            Else { New-VHD -Path "$VHDPath\$Computer\$Computer-$($VHD.Name).vhdx" -SizeBytes $SizeBytes -ComputerName $HypervisorFQDN }
+        
+                            Add-VMHardDiskDrive -VMName $Computer -Path "$($Hypervisor.VHDPath)\$Computer\$Computer-$($VHD.Name).vhdx" -ControllerLocation $ControllerLocation -ComputerName $HypervisorFQDN
+                        
+                        # Need to code for multiple NICs
+                        } #>
+                    }
                 }
                 Default { Write-Error "No hypervisor defined for VM $Computer" -RecommendedAction "Please check json configuration" }
-            }
-
-            If ($PSCmdlet.ShouldProcess($Computer)) {
-                #VM
-                Write-Output "Creating new VM instance: $Computer"
-                Write-Verbose "Memory: $MemoryStartupBytes"
-                Write-Verbose "CPU Count: $CPUCount"
-                Write-Verbose "Generation: $Generation"
-                Write-Verbose "Boot Device: $($Machine.BootDevice)"
-                Write-Verbose "Path: $ConfigPath"
-                Write-Verbose "Switch: $($Machine.DefaultNIC.vSwitch)"
-                Write-Verbose "Hypervisor: $HypervisorFQDN"
-                
-                Hyper-V\New-VM -Name $Computer -MemoryStartupBytes $MemoryStartupBytes -Generation $Generation -NoVHD -BootDevice $Machine.BootDevice -Path $ConfigPath -SwitchName $Machine.DefaultNIC.vSwitch -ComputerName $HypervisorFQDN
-                Set-VMProcessor -VMName $Computer -Count $CPUCount -ComputerName $HypervisorFQDN
-                
-                #Default NIC
-                If ( $Machine.DefaultNIC.MACType -eq 'Static') {
-                    Write-Verbose "Setting default NIC to 'STATIC' with MAC address $($Machine.DefaultNIC.Mac)"
-                    Set-VMNetworkAdapter -VMName $Computer -StaticMacAddress $Machine.DefaultNIC.Mac -DhcpGuard $Machine.DefaultNIC.DHCPGuard -RouterGuard $Machine.DefaultNIC.RouterGuard -MacAddressSpoofing $Machine.DefaultNIC.MacAddressSpoofing -ComputerName $HypervisorFQDN
-                }
-                Else {
-                     Set-VMNetworkAdapter -VMName $Computer -DhcpGuard $Machine.DefaultNIC.DHCPGuard -RouterGuard $Machine.DefaultNIC.RouterGuard -MacAddressSpoofing $Machine.DefaultNIC.MacAddressSpoofing -ComputerName $HypervisorFQDN 
-                }
-                
-                Write-Verbose "DHCP Guard: $($Machine.DefaultNIC.DHCPGuard)"
-                Write-Verbose "Router Guard: $($Machine.DefaultNIC.RouterGuard)"
-                Write-Verbose "MAC Address Spoofing: $($Machine.DefaultNIC.MacAddressSpoofing)"
-
-                #VHDs
-                ForEach ($VHD in $Machine.VHDs) {
-                    Write-Output "Creating new VHD(s) for VM instance: $Computer"  
-                    $ControllerLocation = [Int32]$($VHD.ControllerLocation)
-
-                    Switch -Wildcard ( $VHD.SizeBytes ){
-                        '*GB' { $SizeBytes = [uint64]$($VHD.SizeBytes).Replace('GB','') * 1GB }
-                        '*TB' { $SizeBytes = [uint64]$($VHD.SizeBytes).Replace('TB','') * 1TB }
-                        Default { Write-Error -Message "No unit (MB/GB/TB) associated with VHD size in JSON for $Computer"}
-                    }
-
-                    If ($VHD.Type -eq 'Fixed') { 
-                        New-VHD -Path "$VHDPath\$Computer\$Computer-$($VHD.Name).vhdx" -SizeBytes $SizeBytes -Fixed -ComputerName $HypervisorFQDN  
-                    }
-                    Else { New-VHD -Path "$VHDPath\$Computer\$Computer-$($VHD.Name).vhdx" -SizeBytes $SizeBytes -ComputerName $HypervisorFQDN }
-
-                    Add-VMHardDiskDrive -VMName $Computer -Path "$($Hypervisor.VHDPath)\$Computer\$Computer-$($VHD.Name).vhdx" -ControllerLocation $ControllerLocation -ComputerName $HypervisorFQDN
-                
-                # Need to code for multiple NICs
-                }#>
-            }
+            }  
         }
     }
     End {
     }
 }
-
 
 Export-ModuleMember -Function New-VMInstance
